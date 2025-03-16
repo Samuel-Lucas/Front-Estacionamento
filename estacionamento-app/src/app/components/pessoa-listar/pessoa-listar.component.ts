2715274b9d68c7b3c4611551be1260c87d1442e9
Samuel-Lucas <samuel.moreira10@outlook.com>
2025-03-16 15:24:06 -0300

    Desenvolvendo remoção de pessoa

--- estacionamento-app/src/app/components/pessoa-listar/pessoa-listar.component.ts
+++ estacionamento-app/src/app/components/pessoa-listar/pessoa-listar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Pessoa } from 'src/app/models/pessoa.model';
 import { PessoaService } from 'src/app/services/pessoa.services.service';
@@ -13,7 +14,10 @@ export class PessoaListarComponent implements OnInit {
   pessoas: Observable<Pessoa[]> = new Observable<Pessoa[]>();
   colunasTabela = ['Nome', 'Sobrenome', 'Email', 'Telefone', 'Acoes']
 
-  constructor(private pessoaService: PessoaService) { }
+  constructor(
+    private pessoaService: PessoaService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.listarTodasPessoas();
@@ -21,5 +25,18 @@ export class PessoaListarComponent implements OnInit {
 
   listarTodasPessoas() {
     this.pessoas = this.pessoaService.listarPessoas()
+  }
+
+  deletarPessoa(idPessoa: string) {
+    if (confirm("Deseja excluir sua conta ?")) {
+      this.pessoaService.deletarPessoa(idPessoa).subscribe(
+        response => {
+          this.router.navigateByUrl("")
+        },
+        error => {
+          alert("Erro ao excluir conta" + JSON.stringify(error))
+        }
+      )
+    }
   }
 }
