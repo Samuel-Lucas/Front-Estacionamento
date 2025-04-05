d45f6a0cba8fd5ed82813c6f53be766bf07683c5
Samuel-Lucas <samuel.moreira10@outlook.com>
2025-04-05 11:06:52 -0300

    Criando página de login

--- estacionamento-app/src/app/components/home/home.component.ts
+++ estacionamento-app/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup
+  senhaVisivel = false
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ["", Validators.required],
+      senha: ["", Validators.required]
+    })
   }
 
+  toggleVisibility(input: 'senha'): void {
+    if (input === 'senha') {
+      this.senhaVisivel = !this.senhaVisivel
+    }
+  }
 }
--- estacionamento-app/src/app/modules/home/home.module.ts
+++ estacionamento-app/src/app/modules/home/home.module.ts
@@ -5,6 +5,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from 'src/app/components/home/home.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +20,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     CommonModule,
     HomeRoutingModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ]
 })
 export class HomeModule { }
