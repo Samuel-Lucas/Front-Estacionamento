69bfda63635cd65df4f5fa81dc2e7b302cafcbf5
Samuel-Lucas <samuel.moreira10@outlook.com>
2025-04-16 17:49:06 -0300

    Requisição de endpoints com autorização por token

--- estacionamento-app/src/app/services/pessoa.services.service.ts
+++ estacionamento-app/src/app/services/pessoa.services.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Pessoa } from '../models/pessoa.model';
 import { PessoaInsertRequest } from '../models/pessoa-insert-request.model';
+import { AuthService } from './auth-service.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +13,18 @@ export class PessoaService {
   private baseUrl = 'http://localhost:5122'
   private endpoint = 'api/Pessoa/v1'
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private auth: AuthService) { }
 
   listarPessoas(): Observable<Pessoa[]> {
-    return this.httpClient.get<Pessoa[]>(`${this.baseUrl}/${this.endpoint}`)
+    return this.httpClient.get<Pessoa[]>(`${this.baseUrl}/${this.endpoint}`, {
+      headers: this.getHeaders()
+    })
   }
 
   obterPessoa(idPessoa: string): Observable<Pessoa> {
-    return this.httpClient.get<Pessoa>(`${this.baseUrl}/${this.endpoint}/${idPessoa}`)
+    return this.httpClient.get<Pessoa>(`${this.baseUrl}/${this.endpoint}/${idPessoa}`, {
+      headers: this.getHeaders()
+    })
   }
 
   cadastrarPessoa(pessoa: PessoaInsertRequest): Observable<PessoaInsertRequest> {
@@ -27,10 +32,21 @@ export class PessoaService {
   }
 
   atualizarPessoa(pessoa: Pessoa): Observable<Pessoa> {
-    return this.httpClient.put<Pessoa>(`${this.baseUrl}/${this.endpoint}`, pessoa)
+    return this.httpClient.put<Pessoa>(`${this.baseUrl}/${this.endpoint}`, pessoa, {
+      headers: this.getHeaders()
+    })
   }
 
   deletarPessoa(idPessoa: string): Observable<Pessoa> {
-    return this.httpClient.delete<Pessoa>(`${this.baseUrl}/${this.endpoint}/${idPessoa}`)
+    return this.httpClient.delete<Pessoa>(`${this.baseUrl}/${this.endpoint}/${idPessoa}`, {
+      headers: this.getHeaders()
+    })
+  }
+
+  private getHeaders(): HttpHeaders {
+    const userToken = this.auth.getToken();
+    return new HttpHeaders({
+      Authorization: `Bearer ${userToken}`
+    });
   }
 }
